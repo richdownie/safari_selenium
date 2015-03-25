0af0181dc83607f12acae9cd880edee81a640bb0
rdownie <rich.downie@phishme.com>
2015-03-25 18:15:47 -0400

    fixed

--- signup_today.rb
+++ signup_today.rb
@@ -2,5 +2,4 @@ require 'selenium-webdriver'
 
 @browser = Selenium::WebDriver.for :safari
 @browser.get("http://fullstackautomationwithruby.com")
-@browser.find_element(id: 'user_email').send_keys("vinu.slingmedia@gmail.com")
-puts "Thank you Vinu"
+puts "Signup Today!!!"
